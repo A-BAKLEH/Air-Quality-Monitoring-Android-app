89072c8fc5857b8e638968f2aa066f0d33cfd099
a-bakleh <athanas2000@gmail.com>
2021-11-01 19:48:11 -0400

    Database and LiveMonitoring activity+ UI

---
+++ app/src/main/java/com/example/ceon390_projectgroup/DatabaseActivity.java
@@ -0,0 +1,15 @@
+package com.example.ceon390_projectgroup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DatabaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database);
+    }
+
+}
--- app/src/main/java/com/example/ceon390_projectgroup/MainActivity.java
+++ app/src/main/java/com/example/ceon390_projectgroup/MainActivity.java
@@ -1,79 +1,48 @@
 package com.example.ceon390_projectgroup;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-
 
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView MQ135values;  //variable for text view
-    TextView MQ2values;
-    TextView CO2values;
-    TextView MQ4values;
-    TextView MQ8values;
-    TextView TVOCvalues;
-    TextView MQ9values;
+    Button monitoringButton;
+    Button databaseButton;
 
-    DatabaseReference databaseReference;//variable for database reference for database
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference();  //get instance for firebase
-        MQ135values = findViewById(R.id.MQ135Sensor);  //initialize object class variable
-        MQ2values = findViewById(R.id.MQ2Sensor);
-        MQ4values = findViewById(R.id.MQ4Sensor);
-        CO2values = findViewById(R.id.CO2Sensor);
-        MQ8values = findViewById(R.id.MQ8Sensor);
-        MQ9values = findViewById(R.id.MQ9Sensor);
-        TVOCvalues = findViewById(R.id.TVOCsensor);
+        monitoringButton= findViewById(R.id.monitorringBttn);
+        databaseButton = findViewById(R.id.databaseBtn);
 
-        Read();  //calling method
+        monitoringButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLiveMonitoring();
+            }
+        });
+        databaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToDatabase();
+            }
+        });
     }
 
+    public void goToLiveMonitoring(){
+        Intent intent = new Intent(this, LiveMonitoring.class);
+        startActivity(intent);
+    }
 
-    public void Read() {  //referenced from https://firebase.google.com/docs/database/android/start
-    // Read from the database
-    databaseReference.addValueEventListener(new ValueEventListener() {
-        @Override
-        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-            // This method is called once with the initial value and again
-            // whenever data at this location is updated.
-            String value = dataSnapshot.child("MQ135 Sensor").getValue().toString();
-            MQ135values.setText(value);
-            String value1 = dataSnapshot.child("CO2 CCS811 Sensor").getValue().toString();
-            CO2values.setText(value1);
-            String value2 = dataSnapshot.child("MQ2 Sensor").getValue().toString();
-            MQ2values.setText(value2);
-            String value3 = dataSnapshot.child("MQ4 Sensor").getValue().toString();
-            MQ4values.setText(value3);
-            String value4 = dataSnapshot.child("MQ8 Sensor").getValue().toString();
-            MQ8values.setText(value4);
-            String value5 = dataSnapshot.child("MQ9 Sensor").getValue().toString();
-            MQ9values.setText(value5);
-            String value6 = dataSnapshot.child("TVOC CCS811 Sensor").getValue().toString();
-            TVOCvalues.setText(value6);
-        }
-        @Override
-        public void onCancelled(@NonNull DatabaseError error) {
-            // Failed to read value
-           Toast.makeText(MainActivity.this, "Failed to get data. Error", Toast.LENGTH_SHORT).show();
-        }
-    });
-}
-
+    public void goToDatabase() {
+        Intent intent = new Intent(this, DatabaseActivity.class);
+        startActivity(intent);
+    }
 }
